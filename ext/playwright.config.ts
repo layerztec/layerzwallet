6383b8cb16bd8946bd48a8085a246bdefb32e575
Jude Nullon <evalthis.trade949@passinbox.com>
2025-08-27 14:55:17 +0300

    ci: improve playwright config

--- ext/playwright.config.ts
+++ ext/playwright.config.ts
@@ -15,14 +15,17 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['list', { printSteps: true }], ['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    /* Capture screenshot after each test failure. */
+    screenshot: 'only-on-failure',
+    /* Record video only when retrying a test for the first time. */
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
