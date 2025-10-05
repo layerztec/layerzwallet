1e866314f928b6e748fc8cf2fcb9aa43140aa18a
Tom Heisenberg <m@layerztec.com>
2025-10-05 14:34:18 -0500

    feat: integrate bugsnag

--- mobile/entry.js
+++ mobile/entry.js
@@ -1,11 +1,24 @@
 // Custom entry point for LayerZ Wallet mobile app
 // This replaces expo-router/entry to include necessary polyfills
 
-let Buffer = require('buffer/').Buffer;
-global.Buffer = Buffer;
-
+ 
 import 'react-native-get-random-values';
+
+ 
+import Bugsnag from '@bugsnag/expo';
 
 // should be last
 // @see https://docs.expo.dev/router/installation/
+ 
 import 'expo-router/entry';
+
+let Buffer = require('buffer/').Buffer;
+global.Buffer = Buffer;
+
+const BUGSNAG_API_KEY = process.env.BUGSNAG_API_KEY;
+
+if (BUGSNAG_API_KEY) {
+  Bugsnag.start({
+    apiKey: BUGSNAG_API_KEY,
+  });
+}
