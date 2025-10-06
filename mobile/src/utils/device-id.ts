58678d631fce1e01183dc839d71a6fccad058479
Tom Heisenberg <m@layerztec.com>
2025-10-06 13:54:03 -0500

    fix: dont use platform.select

--- mobile/src/utils/device-id.ts
+++ mobile/src/utils/device-id.ts
@@ -2,21 +2,17 @@ import * as Application from 'expo-application';
 import { Platform } from 'react-native';
 
 export async function getDeviceIdentifier(): Promise<string> {
-  const deviceId = await Platform.select({
-    ios: async () => {
-      const id = await Application.getIosIdForVendorAsync();
-      if (!id) throw new Error('Failed to get iOS device identifier');
-      return id;
-    },
-    android: async () => {
-      const id = Application.getAndroidId();
-      if (!id) throw new Error('Failed to get Android device identifier');
-      return id;
-    },
-    default: async () => {
-      throw new Error('Unsupported platform for device identifier');
-    },
-  })()!;
+  if (Platform.OS === 'ios') {
+    const id = await Application.getIosIdForVendorAsync();
+    if (!id) throw new Error('Failed to get iOS device identifier');
+    return id;
+  }
 
-  return deviceId;
+  if (Platform.OS === 'android') {
+    const id = Application.getAndroidId();
+    if (!id) throw new Error('Failed to get Android device identifier');
+    return id;
+  }
+
+  throw new Error('Unsupported platform for device identifier');
 }
