c5edac44198bcca8ee94ed15dbd21cd23bdf2cec
r1n04h <hello@layerzwallet.com>
2025-10-08 12:38:27 +0100

    fix: more SAR POS terminals suppport

--- shared/modules/merchants.ts
+++ shared/modules/merchants.ts
@@ -33,6 +33,96 @@ export const merchants: MerchantConfig[] = [
       regtest: 'staging.cryptoqr.net',
     },
   },
+  {
+    id: 'yoyo',
+    identifierRegex: /(?<identifier>.*wigroup\.co.*)/iu,
+    defaultDomain: 'cryptoqr.net',
+    domains: {
+      mainnet: 'cryptoqr.net',
+      signet: 'staging.cryptoqr.net',
+      regtest: 'staging.cryptoqr.net',
+    },
+  },
+  {
+    id: 'yoyo2',
+    identifierRegex: /(?<identifier>.*yoyogroup.co.*)/,
+    defaultDomain: 'cryptoqr.net',
+    domains: {
+      mainnet: 'cryptoqr.net',
+      signet: 'staging.cryptoqr.net',
+      regtest: 'staging.cryptoqr.net',
+    },
+  },
+  {
+    id: 'zapper',
+    identifierRegex: /(?<identifier>.*(zapper\.com|\.wigroup\.|payat\.io|paynow\.netcash\.co\.za|paynow\.sagepay\.co\.za|\.zap\.pe|transactionjunction\.co\.za).*)/iu,
+    defaultDomain: 'cryptoqr.net',
+    domains: {
+      mainnet: 'cryptoqr.net',
+      signet: 'staging.cryptoqr.net',
+      regtest: 'staging.cryptoqr.net',
+    },
+  },
+  {
+    id: 'scantopay',
+    identifierRegex: /(?<identifier>.*scantopay\.io.*)/,
+    defaultDomain: 'cryptoqr.net',
+    domains: {
+      mainnet: 'cryptoqr.net',
+      signet: 'staging.cryptoqr.net',
+      regtest: 'staging.cryptoqr.net',
+    },
+  },
+  {
+    id: 'scantopay-numeric',
+    identifierRegex: /^(?<identifier>\d{10})$/,
+    defaultDomain: 'cryptoqr.net',
+    domains: {
+      mainnet: 'cryptoqr.net',
+      signet: 'staging.cryptoqr.net',
+      regtest: 'staging.cryptoqr.net',
+    },
+  },
+  {
+    id: 'Checkers/Shoprite',
+    identifierRegex: /(?<identifier>.*za.co.electrum.*)/,
+    defaultDomain: 'cryptoqr.net',
+    domains: {
+      mainnet: 'cryptoqr.net',
+      signet: 'staging.cryptoqr.net',
+      regtest: 'staging.cryptoqr.net',
+    },
+  },
+  {
+    id: 'snapscan',
+    identifierRegex: /(?<identifier>.*snapscan.*)/,
+    defaultDomain: 'cryptoqr.net',
+    domains: {
+      mainnet: 'cryptoqr.net',
+      signet: 'staging.cryptoqr.net',
+      regtest: 'staging.cryptoqr.net',
+    },
+  },
+  {
+    id: 'Ecentric T1/T2 retailers',
+    identifierRegex: /(?<identifier>.*za.co.ecentric.*)/,
+    defaultDomain: 'cryptoqr.net',
+    domains: {
+      mainnet: 'cryptoqr.net',
+      signet: 'staging.cryptoqr.net',
+      regtest: 'staging.cryptoqr.net',
+    },
+  },
+  {
+    id: 'moneybadger',
+    identifierRegex: /(?<identifier>.*cryptoqr.net.*)/,
+    defaultDomain: 'cryptoqr.net',
+    domains: {
+      mainnet: 'cryptoqr.net',
+      signet: 'staging.cryptoqr.net',
+      regtest: 'staging.cryptoqr.net',
+    },
+  },
 ];
 
 export const convertMerchantQRToLightningAddress = ({ qrContent, network }: { qrContent: string; network: Network }): string | null => {
