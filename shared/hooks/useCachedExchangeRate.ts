019db85669d9f49a97edf9348142fc52c99fcfec
r1n04h <hello@layerzwallet.com>
2026-01-15 15:21:42 +0000

    fix: stacks usd value on network selector

--- shared/hooks/useCachedExchangeRate.ts
+++ shared/hooks/useCachedExchangeRate.ts
@@ -1,5 +1,5 @@
 import { useSWRConfig } from 'swr';
-import { NETWORK_ARK, NETWORK_BITCOIN, NETWORK_BOTANIX, NETWORK_LIGHTNING, NETWORK_LIQUID, NETWORK_ROOTSTOCK, NETWORK_SPARK, NETWORK_USDT, Networks } from '../types/networks';
+import { NETWORK_ARK, NETWORK_BITCOIN, NETWORK_BOTANIX, NETWORK_LIGHTNING, NETWORK_LIQUID, NETWORK_ROOTSTOCK, NETWORK_SPARK, NETWORK_STACKS, NETWORK_USDT, Networks } from '../types/networks';
 import { TFiat } from './useExchangeRate';
 import { StringNumber } from '../types/string-number';
 
@@ -34,6 +34,7 @@ export function useCachedExchangeRate(network: Networks, fiat: TFiat): { exchang
     case NETWORK_LIQUID:
     case NETWORK_BOTANIX:
     case NETWORK_ROOTSTOCK:
+    case NETWORK_STACKS:
       network2use = NETWORK_BITCOIN;
   }
   //
